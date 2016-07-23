6546cb7785ae4404a61e7c9a1e118b4a8543b031
Boris Kraportov <boris.kraportov@gmail.com>
2016-07-24 01:50:30 +0300

    fix postinstall 2

--- scripts/installers/events.bash
+++ scripts/installers/events.bash
@@ -4,15 +4,3 @@ cd taiga-events
 apt-get install -y nodejs nodejs-legacy npm
 npm install
 npm install -g coffee-script
-
-cd $start_point
-mkdir -p /var/log/taiga
-cp ../conf/nginx.conf /etc/nginx/conf.d/$domain.d/$app.conf
-cp ../conf/circus.service /etc/systemd/system/circus.service
-systemctl daemon-reload
-yunohost service add circus
-yunohost service add rabbitmq
-yunohost service add redis-server
-yunohost service add postgresql
-service circus start
-service nginx reload
