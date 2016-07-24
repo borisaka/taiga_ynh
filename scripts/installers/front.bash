a036e6d82cbafaee57044292633c6efa38e1f1b3
Boris Kraportov <boris.kraportov@gmail.com>
2016-07-24 05:46:59 +0300

    fix copy config front

--- scripts/installers/front.bash
+++ scripts/installers/front.bash
@@ -3,4 +3,4 @@ git clone https://github.com/taigaio/taiga-front-dist.git taiga-front-dist
 cd taiga-front-dist
 git checkout stable
 cd $start_point
-cp ../conf/taiga-front.json /taiga-front-dist/dist/conf.json
+cp ../conf/taiga-front.json /var/www/taiga/taiga-front-dist/dist/conf.json
